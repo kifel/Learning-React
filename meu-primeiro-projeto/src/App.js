9b9f259fe147e11c39de9c7d7049421784239c71
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-26 21:06:51 -0300

    Criando components

--- meu-primeiro-projeto/src/App.js
+++ meu-primeiro-projeto/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-
 import './App.css';
+import Frase from './components/Frase';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+  const name = 'kifel'
+
+  function sum (a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meu Primeiro App</h1>
+      <h2>Olá, {name}</h2>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt="minha imagem" />
+      <HelloWorld />
+      <Frase />
     </div>
   );
 }
