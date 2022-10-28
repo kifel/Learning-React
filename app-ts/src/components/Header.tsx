4e1e5b037152b48e0a23f8e9fc8912d0b1ca00f5
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-28 15:07:21 -0300

    header finalizado

--- app-ts/src/components/Header.tsx
+++ app-ts/src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import igniteLogo from '../assets/images/ignite-logo.svg';
 import styles from './Header.module.css';
 
 export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logotipo do ignite" />
     </header>
   );
 }
