9d72f116387eded7a6f00887a60c6dab1a9eee5c
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-27 22:58:00 -0300

    melhorando Tsx

--- my-vite-app/src/App.tsx
+++ my-vite-app/src/App.tsx
@@ -1,13 +1,15 @@
-import './App.css'
-import { User } from './pages/Perfil'
+import "./App.css";
+import { User } from "./pages/Perfil";
 
 function App() {
-
   return (
     <div className="App">
-     <User name="kifel" foto="https://avatars.githubusercontent.com/u/61091742?v=4" />
+      <User
+        name="kifel"
+        foto="https://avatars.githubusercontent.com/u/61091742?v=4"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-vite-app/src/pages/Perfil/index.tsx
+++ my-vite-app/src/pages/Perfil/index.tsx
@@ -1,13 +1,14 @@
-interface Props {
-    name: string;
-    foto: string | undefined;
-}
-export const User: React.FC<Props> = ({ name, foto }) =>{
-    return (
-        <div>
-            <p>Nome do Usuário: {name}</p>
-            <img src={foto} alt={name} />
-            <p>Bem vindo ao sistema</p>
-        </div>
-    );
-}
+type Props = {
+  name: string;
+  foto: string | undefined;
+};
+
+export const User: React.FC<Props> = ({ name, foto }) => {
+  return (
+    <div>
+      <p>Nome do Usuário: {name}</p>
+      <img src={foto} alt={name} />
+      <p>Bem vindo ao sistema</p>
+    </div>
+  );
+};
