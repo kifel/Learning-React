e71b9774024646613c20930155de62d438b519e8
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-29 10:41:29 -0300

    iniciando Post

--- app-ts/src/App.tsx
+++ app-ts/src/App.tsx
@@ -1,19 +1,26 @@
-import styles from './App.module.css';
-import { Header } from './components/Header';
-import { SideBar } from './components/Sidebar';
-import './styles.css';
+import styles from "./App.module.css";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { SideBar } from "./components/Sidebar";
+import "./styles.css";
 
 function App() {
   return (
     <div>
       <Header />
-
       <div className={styles.wrapper}>
         <SideBar />
-        <main></main>
+        <main>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
